22d12832bc116a1b8ed422eec66e269e020cc538
Jose Gonzalez <jdgg2414@gmail.com>
2023-09-06 17:03:19 -0500

    used eval() for load remote js code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,14 +7,48 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 import { onRequest } from 'firebase-functions/v2/https';
-import * as storage from 'firebase-functions/v2/storage';
+import { initializeApp } from 'firebase-admin/app';
 import * as logger from 'firebase-functions/logger';
+import { getStorage } from 'firebase-admin/storage';
+
+initializeApp();
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest(async (request, response) => {
   logger.info('Hello logs!', { structuredData: true });
-  console.log(storage);
-  response.send('Hello world!');
+
+  /**
+   * Get a reference to the storage service, which is used to create references in your storage bucket
+   */
+  const bucket = getStorage().bucket();
+
+  /**
+   * Get a reference to the file you want to download
+   */
+  const file = bucket.file('hello-from-storage.js');
+
+  /**
+   * Create a reference to a local file
+   */
+  const content = await file.download().catch((error) => {
+    logger.error(error);
+  });
+
+  if (content === undefined) {
+    response.send('Error loading the GCS-Hosted file');
+    return;
+  }
+
+  // Convert the buffer to a string
+  const value = content[0].toString('utf-8');
+
+  // eval the string to get the module object
+  const moduleObj = eval(value);
+
+  logger.info(moduleObj, { structuredData: true });
+
+  // send the response
+  response.send(moduleObj.main());
 });
