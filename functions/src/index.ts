371aa9385198f7ce93f779c00057da6d01e5c950
Jose Gonzalez <jdgg2414@gmail.com>
2023-09-12 15:12:39 -0500

    added storage events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
 import { initializeApp } from 'firebase-admin/app';
 import * as logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
+import {
+  onObjectDeleted,
+  onObjectFinalized,
+} from 'firebase-functions/v2/storage';
+import { unlink, writeFile } from 'fs/promises';
 import { switchMap } from 'rxjs';
 import { CloudExecuteFactory } from './app/executable/CloudExecuteFactory';
 import { CloudStorage } from './app/storage/CloudStorage';
+import path = require('path');
+import { existsSync } from 'fs';
 
 initializeApp();
 
@@ -38,3 +45,40 @@ export const cfExecute = onRequest((request, response) => {
       },
     });
 });
+
+export const downloadFile = onObjectFinalized(
+  { bucket: defaultBucket },
+  async (event) => {
+    const file = await storage.getFile(event.data.name, event.bucket);
+    const tempFolder = path.resolve('./temp');
+    const filePath = path.join(tempFolder, event.data.name);
+
+    try {
+      await writeFile(filePath, file.stream());
+    } catch (e) {
+      logger.error(`Error writing file ${filePath}`, e);
+    }
+  },
+);
+
+export const deleteFile = onObjectDeleted(
+  { bucket: defaultBucket },
+  async (event) => {
+    logger.info(`File ${event.data.name} deleted.`);
+    const tempFolder = path.resolve('./temp');
+    const filePath = path.join(tempFolder, event.data.name);
+
+    if (!existsSync(filePath)) {
+      logger.info(`File ${filePath} does not exist.`);
+      return;
+    }
+
+    try {
+      logger.info(`Deleting file ${filePath}`);
+      await unlink(filePath);
+      logger.info(`File ${filePath} deleted.`);
+    } catch (e) {
+      logger.error(`Error deleting file ${filePath}`, e);
+    }
+  },
+);
